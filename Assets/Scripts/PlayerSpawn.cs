47a8bbda8585a971b1997514735f1bd103f75725
Emma Brantley <brantled@mail.uc.edu>
2024-04-15 17:10:39 -0400

    SPAWN

--- Assets/Scripts/PlayerSpawn.cs
+++ Assets/Scripts/PlayerSpawn.cs
@@ -10,7 +10,7 @@ using UnityEngine.UIElements;
 public class PlayerSpawn : NetworkBehaviour
 {
     [SerializeField] public GameObject playerPrefab;
-
+    [SerializeField] public GameObject[] spawnPoints;
     //private GameObject mPlayer;
 
     private void Start()
@@ -18,22 +18,29 @@ public class PlayerSpawn : NetworkBehaviour
         DontDestroyOnLoad(this.gameObject);
     }
 
+    private void Awake()
+    {
+        spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
+    }
+
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
         if (base.IsHost)
         {
+            Debug.Log("Spawn Points" + spawnPoints.Length);
             NetworkManager.Singleton.SceneManager.OnLoadEventCompleted += SpawnPlayer;
         }
     }
 
     public void SpawnPlayer(string sceneName, LoadSceneMode loadSceneMode, List<ulong> clientsCompleted, List<ulong> clientsTimedOut)
     {
+        int spawn = UnityEngine.Random.Range(0, spawnPoints.Length);
         if (IsHost)
         {
             foreach(ulong id in clientsCompleted)
             {
-                GameObject player = Instantiate(playerPrefab);
+                GameObject player = Instantiate(playerPrefab, spawnPoints[spawn].transform.position,Quaternion.identity);
                 player.GetComponent<NetworkObject>().SpawnAsPlayerObject(clientsCompleted.First(), true);
             }
             //GameObject[] spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");    
